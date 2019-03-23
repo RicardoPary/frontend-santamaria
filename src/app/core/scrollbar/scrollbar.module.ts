efde813b6a2d3348a861e8e97c281cab4a3bb728
Ricardo <rikiricardo16@hotmail.com>
2019-03-23 10:56:24 -0800

    styles

--- src/app/core/scrollbar/scrollbar.module.ts
+++ src/app/core/scrollbar/scrollbar.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ScrollbarComponent} from './scrollbar.component';
-import {ScrollbarService} from './scrollbar.service';
 
 @NgModule({
   imports: [
