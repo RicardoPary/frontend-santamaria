f96460cae6410b1fb8b34f8f927da8ecc8e15275
Ricardo <rikiricardo16@hotmail.com>
2019-03-23 11:53:23 -0800

    styles

--- src/app/core/toolbar/search/search.component.ts
+++ src/app/core/toolbar/search/search.component.ts
@@ -1,10 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { MatFormField } from '@angular/material';
 
 @Component({
   selector: 'elastic-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  templateUrl: './search.component.html'
 })
 export class SearchComponent implements OnInit {
 
