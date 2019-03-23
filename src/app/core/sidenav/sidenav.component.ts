b2295c8ee0b2fe198386c96073b8331171620902
Ricardo <rikiricardo16@hotmail.com>
2019-03-23 10:07:41 -0800

    order code

--- src/app/core/sidenav/sidenav.component.ts
+++ src/app/core/sidenav/sidenav.component.ts
@@ -1,13 +1,58 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { SidenavItem } from './sidenav-item/sidenav-item.model';
-import { Observable } from 'rxjs';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {SidenavItem} from './sidenav-item/sidenav-item.model';
+import {Observable} from 'rxjs';
 import * as fromRoot from '../../reducers/index';
-import { Store } from '@ngrx/store';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'elastic-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  template: `
+    <div class="sidenav" fxLayout="column">
+
+      <!-- Layout: Alpha -->
+      <div class="header" fxLayout="row" fxLayoutAlign="start center" *ngIf="layout === 'alpha'">
+
+        <img class="logo" alt="logo" src="assets/img/logo2x.png">
+        <span fxFlex></span>
+
+        <div class="actions" fxLayout="row" fxLayoutAlign="start center">
+          <button class="toggle" type="button" mat-icon-button (click)="toggleSidenavCollapse()" *ngIf="collapsed">
+            <mat-icon>radio_button_unchecked</mat-icon>
+          </button>
+          <button class="toggle" type="button" mat-icon-button (click)="toggleSidenavCollapse()" *ngIf="!collapsed">
+            <mat-icon>radio_button_checked</mat-icon>
+          </button>
+        </div>
+      </div>
+      <!-- /Layout: Alpha -->
+
+      <elastic-scrollbar class="menu" fxFlex>
+
+        <div class="heading" fxLayout="row" fxLayoutAlign="space-between center">
+          <div class="name">Navigation</div>
+
+          <!-- Layout: Beta -->
+          <div class="actions" *ngIf="layout === 'beta'">
+            <button class="toggle" type="button" mat-icon-button (click)="toggleSidenavCollapse()" *ngIf="collapsed">
+              <mat-icon>radio_button_unchecked</mat-icon>
+            </button>
+            <button class="toggle" type="button" mat-icon-button (click)="toggleSidenavCollapse()" *ngIf="!collapsed">
+              <mat-icon>radio_button_checked</mat-icon>
+            </button>
+          </div>
+          <!-- /Layout: Beta -->
+
+        </div>
+
+        <div class="sidenav-item-holder" *ngFor="let item of sidenavItems$ | async">
+          <elastic-sidenav-item class="sidenav-item-container" [item]="item"
+                                [currentlyOpen]="currentlyOpen$ | async"></elastic-sidenav-item>
+        </div>
+
+      </elastic-scrollbar>
+
+    </div>
+  `
 })
 export class SidenavComponent implements OnInit, OnDestroy {
 
@@ -20,7 +65,8 @@ export class SidenavComponent implements OnInit, OnDestroy {
 
   constructor(
     private store: Store<fromRoot.State>
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.sidenavItems$ = this.store.select(fromRoot.getSidenavItems);
--- src/app/core/sidenav/sidenav.module.ts
+++ src/app/core/sidenav/sidenav.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SidenavComponent } from './sidenav.component';
-import { SidenavItemComponent } from './sidenav-item/sidenav-item.component';
-import { SidenavCollapseDirective } from './sidenav-collapse.directive';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatIconModule, MatRippleModule } from '@angular/material';
-import { ScrollbarModule } from '../scrollbar/scrollbar.module';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SidenavComponent} from './sidenav.component';
+import {SidenavItemComponent} from './sidenav-item/sidenav-item.component';
+import {SidenavCollapseDirective} from './sidenav-collapse.directive';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule, MatIconModule, MatRippleModule} from '@angular/material';
+import {ScrollbarModule} from '../scrollbar/scrollbar.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
@@ -28,4 +28,5 @@ import { RouterModule } from '@angular/router';
     SidenavCollapseDirective
   ]
 })
-export class SidenavModule { }
+export class SidenavModule {
+}
