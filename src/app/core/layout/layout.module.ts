4fcac8ce6ec6b19f8c099e3dfd3f4fd3e6f6d062
Ricardo <rikiricardo16@hotmail.com>
2019-04-15 14:24:14 -0800

    clean code

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatIconModule, MatRippleModule, MatSidenavModule } from '@angular/material';
-import { SettingsModule } from 'app/core/settings/settings.module';
-import { SidenavModule } from '../sidenav/sidenav.module';
-import { ToolbarModule } from '../toolbar/toolbar.module';
-import { QuickpanelModule } from '../quickpanel/quickpanel.module';
-import { RouterModule } from '@angular/router';
-import { ScrollbarService } from '../scrollbar/scrollbar.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LayoutComponent} from './layout.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule, MatIconModule, MatRippleModule, MatSidenavModule} from '@angular/material';
+import {SettingsModule} from 'app/core/settings/settings.module';
+import {SidenavModule} from '../sidenav/sidenav.module';
+import {ToolbarModule} from '../toolbar/toolbar.module';
+import {QuickpanelModule} from '../quickpanel/quickpanel.module';
+import {RouterModule} from '@angular/router';
+import {ScrollbarService} from '../scrollbar/scrollbar.service';
 
 @NgModule({
   imports: [
@@ -28,4 +28,5 @@ import { ScrollbarService } from '../scrollbar/scrollbar.service';
   exports: [LayoutComponent],
   providers: [ScrollbarService]
 })
-export class LayoutModule { }
+export class LayoutModule {
+}
